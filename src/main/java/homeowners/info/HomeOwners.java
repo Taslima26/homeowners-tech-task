6bf2f660b1e7360fdca12f6ba89de2bae072a6c3
taslima26 <banglawalas@hotmail.co.uk>
2021-04-26 00:19:18 +0100

    removed unnecessary getters and setters

--- src/main/java/homeowners/info/HomeOwners.java
+++ src/main/java/homeowners/info/HomeOwners.java
@@ -16,6 +16,7 @@ public class HomeOwners {
     private String initial2;
 
 
+
     public HomeOwners(String title1, String firstName1, String lastName1, String initial1) {
         this.title1 = title1;
         this.firstName1 = firstName1;
@@ -34,69 +35,7 @@ public class HomeOwners {
         this.initial2 = initial2;
     }
 
-    public String getTitle1() {
-        return title1;
-    }
 
-    public void setTitle1(String title1) {
-        this.title1 = title1;
-    }
-
-    public String getFirstName1() {
-        return firstName1;
-    }
-
-    public void setFirstName1(String firstName1) {
-        this.firstName1 = firstName1;
-    }
-
-    public String getInitial1() {
-        return initial1;
-    }
-
-    public void setInitial1(String initial1) {
-        this.initial1 = initial1;
-    }
-
-    public String getLastName1() {
-        return lastName1;
-    }
-
-    public void setLastName1(String lastName1) {
-        this.lastName1 = lastName1;
-    }
-
-    public String getTitle2() {
-        return title2;
-    }
-
-    public void setTitle2(String title2) {
-        this.title2 = title2;
-    }
-
-    public String getFirstName2() {
-        return firstName2;
-    }
-
-    public void setFirstName2(String firstName2) {
-        this.firstName2 = firstName2;
-    }
-
-    public String getLastName2() {
-        return lastName2;
-    }
-
-    public void setLastName2(String lastName2) {
-        this.lastName2 = lastName2;
-    }
-
-    public String getInitial2() {
-        return initial2;
-    }
-
-    public void setInitial2(String initial2) {
-        this.initial2 = initial2;
-    }
 
     @Override
     public String toString() {
